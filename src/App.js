5620eaa81355e2955fbed623d0aaa2ad49984bbd
eddie0mohammed <me3dun@gmail.com>
2019-08-07 13:14:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Homepage from './components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
